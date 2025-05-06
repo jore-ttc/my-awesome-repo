fb6e2bafc9007e699c5154e25ed54bb113c3b4dd
Hasse Feldthaus <hafe@foa.dk>
2025-05-06 14:06:21 +0200

    Add initial implementation of Demo class with main method

---
+++ f4/demo.py
@@ -0,0 +1,8 @@
+class Demo:
+    @staticmethod
+    def main():
+        print("This is a test for trying out pull requests.")
+
+# To execute the main method
+if __name__ == "__main__":
+    Demo.main()
